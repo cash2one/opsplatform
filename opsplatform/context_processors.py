1c6872b77c1672925604d9e46db077227616e92f
lrqrun <lrqrun@gmail.com>
2016-07-04 14:48:16 +0800

    bug fix remove url

--- opsplatform/context_processors.py
+++ opsplatform/context_processors.py
@@ -6,10 +6,8 @@ from MySQLdb import ProgrammingError
 
 
 def name_proc(request):
-    dashboard_url = settings.DASHBOARD_URL
     portal_url = settings.PORTAL_URL
     alarm_url = settings.ALARM_URL
-    grafana_url = settings.GRAFANA_URL
 
     user_total_num = User.objects.all().count()
     user_active_num = User.objects.filter(is_active=True).count()
@@ -18,10 +16,8 @@ def name_proc(request):
     request.session.set_expiry(3600)
 
     info_dic = {
-                'dashboard_url': dashboard_url,
                 'portal_url': portal_url,
                 'alarm_url': alarm_url,
-                'grafana_url': grafana_url,
                 'user_total_num': user_total_num,
                 'user_active_num': user_active_num,
                 'host_total_num': host_total_num,
